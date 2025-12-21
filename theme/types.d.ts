d6812ac39362018c27d9d8628f427c2ea6635f10
Andrew Wittrock <andy@wittrock.us>
2025-12-21 01:03:13 -0600

    Update theme types

--- theme/types.d.ts
+++ theme/types.d.ts
@@ -1,11 +1,13 @@
+import { ILayoutComponent, ITheme as IThemeDef } from "@core/lib/layout/layout";
 import { NewObj } from "../../core-shared/express/types";
 
 export declare interface ITheme {
     id: string;
     name: string;
     description: string;
-    imageUrl: string;
-    json: string;
+    imageUrl: string | null;
+    json: IThemeDef | null;
+    enabled: boolean;
 }
 
 export type NewTheme = NewObj<ITheme>;
